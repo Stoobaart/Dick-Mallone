229d006b86e9a0706209b6659e4e042a57da2a73
Stuart Smith <stuartgordonsmith5@gmail.com>
2018-02-09 21:01:54 +0000

    pedestrians added to skyway entrance scene

--- app/components/skyway-scene.js
+++ app/components/skyway-scene.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { get, set } from '@ember/object';
+import { later } from '@ember/runloop';
 import $ from 'jquery';
 
 export default Component.extend({
@@ -15,11 +16,39 @@ export default Component.extend({
     }
     $(".skyway-entrance-scene, .rain-container-skyway").fadeIn(1000);
     $("#player").fadeIn(500);
-    $('#skywaySceneMusic')[0].play();
+    later(() => {
+      $('#skywaySceneMusic')[0].play();
+      $("#rainSoundFx")[0].play();
+    }, 50);
+    this.sendAction('playerSpeach', "..I hate this place..")
+    this.animatepedestrian1();
+    this.animatepedestrian2();
+    setInterval(() => {
+      this.animatepedestrian1();
+    }, 12050);
+    setInterval(() => {
+      this.animatepedestrian2();
+    }, 13050);
   },
 
   willDestroyElement() {
     $('#skywaySceneMusic')[0].pause();
   },
+
+  animatepedestrian1() {
+    $('#pedestrian1').show().animate({
+      left: -50
+    }, 12000, function() {
+      $('#pedestrian1').hide().css("left", "81rem");
+    });
+  },
+
+  animatepedestrian2() {
+    $('#pedestrian2').show().animate({
+      left: 1550
+    }, 12000, function() {
+      $('#pedestrian2').hide().css("left", "-3rem");
+    });
+  }
 
 });
