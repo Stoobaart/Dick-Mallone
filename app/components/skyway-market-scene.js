d637022da83a345a312370dfeb52164960a04f37
Stuart Smith <stuartgordonsmith5@gmail.com>
2018-02-11 10:55:01 +0000

    bg image for skyway markets added, with pedestrians

--- app/components/skyway-market-scene.js
+++ app/components/skyway-market-scene.js
@@ -10,65 +10,40 @@ export default Component.extend({
   pedestrians: Ember.A([
     {
       "imgId": "ped1",
-      "containerId": "pedestrian1",
+      "containerId": "pedestrian1-market",
       "filePath": "sprites/Pedestrian.png",
       "startPos": "81rem",
       "endPos": -50,
-      "time": 12000
+      "time": 13000
     },{
       "imgId": "ped2",
-      "containerId": "pedestrian2",
+      "containerId": "pedestrian2-market",
       "filePath": "sprites/Pedestrian2.png",
       "startPos": "-3rem",
       "endPos": 1550,
       "time": 13000
-    },{
+    },{     
       "imgId": "ped3",
-      "containerId": "pedestrian3",
+      "containerId": "pedestrian3-market",
       "filePath": "sprites/Pedestrian3.png",
       "startPos": "81rem",
       "endPos": -50,
       "time": 11000
-    },{
-      "imgId": "ped4",
-      "containerId": "pedestrian4",
-      "filePath": "sprites/Pedestrian3.png",
-      "startPos": "81rem",
-      "endPos": -50,
-      "time": 9000
-    },{
-      "imgId": "ped5",
-      "containerId": "pedestrian5",
-      "filePath": "sprites/Pedestrian5.png",
-      "startPos": "-3rem",
-      "endPos": 1550,
-      "time": 14000
-    },{
-      "imgId": "ped6",
-      "containerId": "pedestrian6",
-      "filePath": "sprites/Pedestrian6.png",
-      "startPos": "81rem",
-      "endPos": -50,
-      "time": 16000
     }
   ]),
 
   didInsertElement() {
     this._super(...arguments);
     $('#player').stop();
-    set(this, 'scene', 'skyway-scene');
-    if (get(this, 'state.previousScene') === 'car-scene') {
+    set(this, 'scene', 'skyway-market-scene');
+    if (get(this, 'state.previousScene') === 'skyway-scene') {
       $("#player").css({top: 441, left: 108}).html('<img class="playerSprite" src="sprites/dickRight.png">');
-    } else if (get(this, 'state.previousScene') === 'skyway-market-scene') {
-      $("#player").css({top: 441, left: 1208}).html('<img class="playerSprite" src="sprites/dickLeft.png">');
     }
-    $(".skyway-entrance-scene, .rain-container-skyway").fadeIn(1000);
+    $(".skyway-market-bg-scene, .skyway-market-fg-scene, .rain-container-skyway").fadeIn(1000);
     $("#player").fadeIn(500);
     later(() => {
       $('#skywaySceneMusic')[0].play();
-      $("#rainSoundFx")[0].play();
     }, 50);
-    this.sendAction('playerSpeach', "..I hate this place..")
     this.animatepedestrian(get(this, 'pedestrians')[0]);
     this.animatepedestrian(get(this, 'pedestrians')[1]);
     get(this, 'pedestrians').forEach((ped) => {
@@ -77,10 +52,6 @@ export default Component.extend({
         this.animatepedestrian(ped);
       }, intervalTime);
     });
-  },
-
-  willDestroyElement() {
-    $('#skywaySceneMusic')[0].pause();
   },
 
   animatepedestrian(ped) {
@@ -92,5 +63,6 @@ export default Component.extend({
       $(`#${ped.containerId}`).css("left", `${ped.startPos}`);
     });
   }
+
 
 });
--- tests/integration/components/skyway-market-scene-test.js
+++ tests/integration/components/skyway-market-scene-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('skyway-markets-scene', 'Integration | Component | skyway markets scene', {
+moduleForComponent('skyway-market-scene', 'Integration | Component | skyway markets scene', {
   integration: true
 });
 
@@ -9,7 +9,7 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{skyway-markets-scene}}`);
+  this.render(hbs`{{skyway-market-scene}}`);
 
   assert.equal(this.$().text().trim(), '');
 
